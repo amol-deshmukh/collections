e58c17a7153b92ce9cf42a61235b7daa5c090501
Amol Deshmukh <amold86@gmail.com.>
2015-03-28 18:41:00 +0530

    Added Methods

--- Collections/src/com/ghatnandurkar/utils/Utils.java
+++ Collections/src/com/ghatnandurkar/utils/Utils.java
@@ -1,5 +1,19 @@
 package com.ghatnandurkar.utils;
 
+import java.util.HashMap;
+
 public class Utils {
+
+	public void testCloneOfMap(){
+		HashMap<String, Integer> map= new HashMap<String, Integer>();
+		
+		map.put("Amol", 1);
+		HashMap<String, Integer> map1= new HashMap<String, Integer>();
+		map1=(HashMap<String, Integer>) map.clone();
+		map.clear();
+		System.out.println(map1);
+	}
+	
+	
 
 }
