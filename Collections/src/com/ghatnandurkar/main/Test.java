8cd211c1c2551ecd395190645004b794a97674b1
Amol Deshmukh <amold86@gmail.com.>
2015-03-28 18:41:47 +0530

    ADDED Test.java

---
+++ Collections/src/com/ghatnandurkar/main/Test.java
@@ -0,0 +1,15 @@
+package com.ghatnandurkar.main;
+
+import com.ghatnandurkar.utils.Utils;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		Utils utils= new Utils();
+		utils.testCloneOfMap();
+
+	}
+
+}
